1bcbea5920127129dfa9f78cc92f7f8ecfd194ef
apowledge <apowledge@gmail.com>
2023-01-25 12:39:58 -0700

    html tweaks

--- pathfinder/__init__.py
+++ pathfinder/__init__.py
@@ -5,7 +5,7 @@ Runs on 'from pathfinder import app'
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+#from flask_login import LoginManager
 from flask_gravatar import Gravatar
 
 from pathfinder.config import Config
@@ -15,10 +15,10 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'users.login'
-login_manager.login_message_category = 'info'
+#login_manager = LoginManager(app)
+#login_manager.login_view = 'users.login'
+#login_manager.login_message_category = 'info'
 gravatar = Gravatar(app, size=100, rating='g', default='identicon', force_default=False, use_ssl=False, base_url=None)
 
-
+from pathfinder import routes
 
--- pathfinder/routes.py
+++ pathfinder/routes.py
@@ -1,7 +1,10 @@
+from pathfinder import app
 from flask import render_template, request, Blueprint, send_from_directory
 
 @app.route("/")
+@app.route("/home")
 def home():
-    #return render_template("two-col-left-sidebar.html")
+    return render_template("two-col-left-sidebar.html")
     #return render_template("base_bootstrap.html")
-    return render_template("temp.html")
+    #print("Display Home Page")
+    #return render_template("temp.html")
