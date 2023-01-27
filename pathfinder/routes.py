a98d97c3adc7f079d242b54372931688b6315fa0
apowledge <apowledge@gmail.com>
2023-01-27 15:31:45 -0700

    github page placeholder

--- pathfinder/routes.py
+++ pathfinder/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, request, Blueprint, send_from_directory
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("two-col-left-sidebar.html")
+    #return render_template("two-col-left-sidebar.html")
     #return render_template("base_bootstrap.html")
     #print("Display Home Page")
-    #return render_template("temp.html")
+    return render_template("temp.html")
